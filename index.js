455af155bf1896012e0f6e3f645149a385f93c62
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-04-29 21:46:20 +0600

    initialized mongo and perfumes api

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// perfumaniaAdmin
+// UCUtBYI4vDcH5Utf
+
+// Middleware 
+app.use(cors());
+app.use(express.json());
+
+// MongoDB
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qm6xo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const perfumeCollection = client.db("perfumania").collection("perfumes");
+
+        app.get("/perfumes", async (req, res) => {
+            const query = req.query;
+            console.log(query);
+            const cursor = perfumeCollection.find(query);
+            const perfumes = await cursor.toArray();
+            res.send(perfumes)
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
+    res.send("Perfumania is up and running perfectly");
+})
+
+app.listen(port, () => {
+    console.log('Listening to Perfumania, port', port);
+})
